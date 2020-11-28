bced195bf33a9d868ab22b0eb245874fa841df45
Geonho Kim <gunhoflash@gmail.com>
2020-11-29 03:55:25 +0900

    Visualize bikestops in 2 ways

--- proj2/wifi/app.js
+++ proj2/wifi/app.js
@@ -8,7 +8,7 @@ import DeckGL from '@deck.gl/react';
 import {readString} from "react-papaparse";
 
 // Set your mapbox token here
-const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+const MAPBOX_TOKEN = process.env.MapboxAccessToken;
 
 const ambientLight = new AmbientLight({
   color: [255, 255, 255],
@@ -37,83 +37,90 @@ const material = {
 };
 
 const INITIAL_VIEW_STATE = {
-  longitude: 126.9779,
-  latitude: 37.5663,
-  zoom: 10,
+  longitude: 126.9679,
+  latitude: 37.5203,
+  zoom: 11.5,
   minZoom: 1,
   maxZoom: 15,
-  pitch: 40.5,
-  bearing: -27
+  pitch: 52,
+  bearing: 15
 };
 
 // 더 많은 세팅: https://colorbrewer2.org
 // set "Number of data classes" to 6
 export const colorRange = [
-  [255, 255, 255, 30],
-  [255, 255, 255, 60],
-  [255, 255, 255, 90],
-  [255, 255, 255, 120],
-  [255, 255, 255, 150],
-  [255, 255, 255, 180],
-  [255, 255, 255, 210],
+  [226, 232, 255, 40],
+  [226, 232, 255, 70],
+  [226, 232, 255, 100],
+  [226, 232, 255, 130],
+  [226, 232, 255, 160],
+  [226, 232, 255, 190],
+  [226, 232, 255, 220],
 ];
 
 function getTooltip({object}) {
   if (!object) {
     return null;
   }
-  const lat = object.position[1];
-  const lng = object.position[0];
-  const count = object.points.length;
 
-  return `\
-    latitude: ${Number.isFinite(lat) ? lat.toFixed(6) : ''}
-    longitude: ${Number.isFinite(lng) ? lng.toFixed(6) : ''}
-    ${count} Accidents`;
+  if (object.position && object.points) {
+    // hexagon
+    const lat = object.position[1];
+    const lng = object.position[0];
+    const count = object.points.length;
+  
+    return {
+      html: `\
+        <strong>총 ${count}곳</strong><br><br>
+        <small>
+          (${Number.isFinite(lat) ? lat.toFixed(6) : ''}, ${Number.isFinite(lng) ? lng.toFixed(6) : ''})
+        </small>
+        `
+    };
+  } else {
+    return {
+      html: `\
+        <strong>[${object[0]}] ${object[1]}</strong><br>
+        <small>
+        ${object[9]}방식 ${object[7] + object[8]}대<br><br>
+        ${object[3]}<br>
+        (${object[4]}, ${object[5]})
+        </small>
+        `
+    };
+  }
 }
 
 /* eslint-disable react/no-deprecated */
 export default function App({
   data,
   mapStyle = 'mapbox://styles/mapbox/dark-v9',
-  radius = 800,
-  lowerPercentile = 0,
-  upperPercentile = 100,
-  coverage = 0.86
 }) {
   const layers = [
     // // reference: https://deck.gl/docs/api-reference/aggregation-layers/hexagon-layer
     new HexagonLayer({
       id: 'wifi',
       colorRange,
-      coverage,
+      coverage: 0.92,
       data,
-      // elevationRange: [50, 500],
-      // elevationScale: 5, //data && data.length ? 50 : 0,
       extruded: false,
-      getPosition: d => d,
+      getPosition: d => [Number(d[5]), Number(d[4])],
       pickable: true,
-      radius,
-      // upperPercentile,
+      radius: 800,
       material,
-      // transitions: {
-      //   elevationScale: 50
-      // }
     }),
     new IconLayer({
       id: 'icon-layer',
       data,
       pickable: true,
-      // iconAtlas and iconMapping are required
-      // getIcon: return a string
       iconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
       iconMapping: {
         marker: {x: 0, y: 0, width: 128, height: 128, mask: true, anchorY: 128}
       },
       getIcon: d => 'marker',
       sizeScale: 5,
-      getPosition: d => d,
-      getSize: d => 7,
+      getPosition: d => [Number(d[5]), Number(d[4])],
+      getSize: d => 6,
       getColor: d => [0, 192, 72]
     }),
 
@@ -140,8 +147,9 @@ export default function App({
 async function renderBikestop(container) {
   // read CSV file
   const BIKESTOP_DATA = await fetch("data/공공자전거 대여소 정보(20.07.13 기준) UTF-8.csv")
-    .then(response => response.text())
-    .then(text => readString(text).data.map(d => [Number(d[5]), Number(d[4])]))
+    .then(fethed => fethed.text())
+    .then(text => readString(text).data) // split text into rows
+    .then(data => data.filter(row => row[0])); // filter invalid row
 
   render(<App data={BIKESTOP_DATA} />, container);
 }
